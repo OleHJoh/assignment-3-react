f3ae3d4817d502213deeddf282d0fb9ffae0d526
OleHJoh <johansen.ole.h@gmail.com>
2022-01-29 18:59:36 +0100

    Added context components to the app

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -2,35 +2,36 @@ import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { loginUser } from "../../api/user";
 import { storageSave } from "../../utils/storage";
-import { useHistory } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { useUser } from "../../context/UserContext";
 
 const usernameConfig = {
   required: true,
   minLength: 3,
 };
 
-const LoginForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const LoginForm = () => { const { register, handleSubmit, formState: { errors }, } = useForm();
+  const { user, setUser } = useUser()
+  const navigate = useNavigate()
 
   const [ loading, setLoading ] = useState(false)
   const [ apiError, setApiError ] = useState(null)
 
   useEffect(() => {
-      
-  }, [])
+      if(user !== null){
+        navigate('profile')
+      }
+  }, [ user, navigate ])
 
   const onSubmit = async ({username}) => {
     setLoading(true)
-    const [error, user] = await loginUser(username)
+    const [error, userResponse] = await loginUser(username)
     if(error !== null){
         setApiError(error)
     }
-    if(user !== null){
-        storageSave('translator-user', user)
+    if(userResponse !== null){
+        storageSave('translator-user', userResponse)
+        setUser(userResponse)
     }
     setLoading(false)
   };
